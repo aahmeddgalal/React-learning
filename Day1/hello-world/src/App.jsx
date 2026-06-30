61eafec3ad83ba0b06d22e7dbaf9d42c7eb195db
Galileo <aahmeddgalal@gmail.com>
2026-06-30 19:06:01 +0300

    day1

---
+++ Day1/hello-world/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { Welcome } from './Welcome'
+import { Button } from './Button'
+
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>React Course</h1>
+      <Welcome />
+      <Button />
+    </>
+  )
+}
+
+export default App
---
+++ Day1/hello-world/src/Welcome.jsx
@@ -0,0 +1,4 @@
+export const Welcome = () => {
+    return <h2>Helloo</h2>;
+}
+
